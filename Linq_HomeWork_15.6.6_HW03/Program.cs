3860fd0032e394db30f32b90100c034151964f55
Dimanqe <d.sankeev@gmail.com>
2023-09-29 02:31:04 +0300

    Добавьте файлы проекта.

---
+++ Linq_HomeWork_15.6.6_HW03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classrooms;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, },
+               new Classroom { Students = {"Bulat2", "Alex2", "Galina2"} },
+               
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(classroom => classroom.Students).ToArray();
+
+        }
+       
+    }
+}
